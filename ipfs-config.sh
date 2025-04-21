a26a0b00a90b3a6079c0a086f3e885e39102c495
Julian Nalenz <julian.nalenz@divizend.com>
2025-04-21 02:58:57 +0200

    fix: fly.toml

--- ipfs-config.sh
+++ ipfs-config.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-# Replace YOUR_IPV4_HERE with your Fly.io public IPv4 address
+# Replace the IPv4 with your Fly.io public IPv4 address
 ipfs config --json Addresses.AppendAnnounce '["/ip4/168.220.83.224/tcp/4001"]'
